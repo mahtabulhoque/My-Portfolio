a769be622e058c27c71f12c5b2d28d823bff4c4e
hagghat <mahach0396@gmail.com>
2024-07-05 00:14:54 +0600

    update education link

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
             </Link>
           </li>
           <li>
-            <Link to="education" 
+            <Link to="educations" 
             smooth={true} offset={50} duration={500}
             className="block px-4 py-2 text-gray-700 hover:bg-gray-100" onClick={toggleDropdown}>
               Education
