4fdf16f0eef9bb1ae87bfb1903d68119e6d80336
hagghat <mahach0396@gmail.com>
2024-07-04 10:36:27 +0600

    updated hero-1

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,39 @@
+import { TypeAnimation } from 'react-type-animation';
+import profilePic from '../assets/file2.png'
+const Hero = () => {
+    return (
+        <div className='grid md:grid-cols-2 place-items-center max-w-[1250px] mx-auto mb-8 md:mb-32 mt-14'>
+            <div className='mzx-w-[800px]'>
+                <p className='text-gray-200 md:text-6xl text-3xl tracking-tight'>
+                    HEY, I AM <br />
+                    <span>MAHTABUL HOQUE CHOWDHURY</span> <br />
+                    <TypeAnimation
+                      sequence={[
+                        "Web-Developer",
+                        1000,
+                        "Tech-Learner",
+                        1000,
+                        "Translator",
+                        1000,
+                        "Writer...",
+                        1000
+                      ]}
+                      speed={50}
+                      repeat={Infinity}
+                      className='font-bold italic'
+                    />
+
+                    
+                </p>
+
+                <div className='flex flex-row gap-4 mb-4 md:mb-0'>
+                    <button className='z-10 cursor-pointer font-bold text-gray-200 w-1/2 mt-6 p-2 bg-gradient-to-r from-[#0e85b4] via-[#083d47] to-[#117583] rounded-xl'>Download CV</button>
+                </div>
+
+            </div>
+            <img className='w-[300px] md:w-[500px]' src={profilePic} alt="profilePic" />
+        </div>
+    );
+};
+
+export default Hero;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,13 +16,29 @@ const Navbar = () => {
   return (
     <div className="flex text-2xl justify-between text-gray-200 items-center px-6 max-w-[1400px] mx-auto h-24">
       <a href="">MAHTAB</a>
+
       <ul className="hidden md:flex gap-12 z-10 cursor-pointer">
-        <li>
-          <Link>About</Link>
+        <li className="relative group">
+          <Link to="about" smooth={true} offset={50} duration={500}>
+            About
+          </Link>
+          <span className="absolute bottom-0 left-0 w-full h-1 bg-sky-600 transform scale-x-0 group-hover:-scale-x-100 transition-transform duration-300 ease-in-out"></span>
+        </li>
+        <li className="relative group">
+          <Link to="portfolio" smooth={true} offset={50} duration={500}>
+            Portfolio
+          </Link>
+          <span className="absolute bottom-0 left-0 w-full h-1 bg-sky-600 transform scale-x-0 group-hover:-scale-x-100 transition-transform duration-300 ease-in-out"></span>
+        </li>
+        <li className="relative group">
+          <Link to="Contact" smooth={true} offset={50} duration={500}>
+            Contact
+          </Link>
+          <span className="absolute bottom-0 left-0 w-full h-1 bg-sky-600 transform scale-x-0 group-hover:-scale-x-100 transition-transform duration-300 ease-in-out"></span>
         </li>
       </ul>
 
-      <div>
+      <div onClick={toggleNav} className="md:hidden z-30">
         {nav ? (
           <AiOutlineClose size={30}></AiOutlineClose>
         ) : (
@@ -30,10 +46,23 @@ const Navbar = () => {
         )}
       </div>
 
-      <div>
+      <div className={nav? 'text-center z-20 fixed h-full w-full left-0 top-0 bg-[#232323]' : 'fixed left-[-100%]'}>
         <ul>
           <li>
-            <Link>About</Link>
+            <Link to="about" onClick={closeNav} smooth={true} offset={50} duration={500}>
+              About
+            </Link>
+          </li>
+
+          <li>
+            <Link to="portfolio" onClick={closeNav} smooth={true} offset={50} duration={500}>
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" onClick={closeNav} smooth={true} offset={50} duration={500}>
+              contact
+            </Link>
           </li>
         </ul>
       </div>
